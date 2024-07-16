50aab5a99ac814b52e92a0148a8287ece65efc47
SatyajitChitnis <154325199+SatyajitChitnis@users.noreply.github.com>
2024-07-16 11:08:47 +0100

    Update index.php - On call Demo

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<h1>This is Test APP Service Deployment</h1>
+<h1>This is Test APP Service Deployment _ Edited File</h1>
 <?php
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
